ed8a077404a9533a93a35d6ed2b159e82b319a50
pawel.smolinski <pawel.smolinski@softwareplant.com>
2024-05-13 16:09:26 +0200

    SearchBar component

---
+++ app/sandbox/page.tsx
@@ -0,0 +1,11 @@
+import { SearchBar } from "@/components/ui/searchBar";
+
+export default function Sandbox() {
+  return (
+    <div>
+      <h1> Components sandbox: </h1>
+      <h2>SearchBar:</h2>
+      <SearchBar />
+    </div>
+  );
+}
