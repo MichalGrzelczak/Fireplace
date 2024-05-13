a1a375477fa367cb5230e3e105e73ab00ff8d588
Piotr Szymanski <piotr.szymanski@softwareplant.com>
2024-05-13 14:03:16 +0200

    login-html

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import Logo from "@/app/components/logo/Logo";
-
 export default function Home() {
   return (
     <div className="h-screen w-screen grid grid-cols-2 overflow-hidden">
-      <div>
-        <Image
-          alt=""
-          src="/left-curve.svg"
-          width={100}
-          height={100}
-          layout="responsive"
-          objectFit="contain"
-        />
-      </div>
-
-      <div className="flex items-center justify-center flex-col">
-        <Logo width={333} height={78} />
-        <h3>Links</h3>
-        <ul>
-          <li>
-            <Link href="/find-team">Find team</Link>
-          </li>
-        </ul>
-      </div>
+      <Link href="/login">Logowanie</Link>
     </div>
   );
 }
