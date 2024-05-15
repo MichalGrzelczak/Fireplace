f3d2c326a5c2bc59a155b356e322c0da79a4db5b
Patryk Krasuski <patryk.krasuski@appfire.com>
2024-05-15 11:14:07 +0200

    middleware changes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
 import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export default withAuth(async (request: NextRequest) => {
   const token = await getToken({ req: request });
   console.log("token:", token);
 
   if (!token && process.env.NEXTAUTH_URL) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-}
+});
 
 export const config = {
   matcher: "/app/:path*",
