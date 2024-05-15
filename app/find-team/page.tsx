3ee68af5db0374ef825d1f54ad795e7e85c32ef0
Michał Grzelczak <michal.grzelczak@softwareplant.com>
2024-05-15 15:12:55 +0200

    Jira to krol zarzadzania projektami, tak jak lew jest krol sawanny

--- app/find-team/page.tsx
+++ app/find-team/page.tsx
@@ -1,3 +1,7 @@
-export default function FindTeam() {
+import { fetchJiraIssues } from "@/app/find-team/jiraApi";
+
+export default async function FindTeam() {
+  const data = await fetchJiraIssues();
+
   return <div>Find team works</div>;
 }
