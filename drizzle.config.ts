31918f376bea8a19c1fccdb833c2709dad0abff8
marcin.hareza <marcin.hareza@softwareplant.com>
2024-05-15 15:40:13 +0200

    Fixed drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
-  driver: "better-sqlite",
   dbCredentials: {
     url: "sqlite.db",
   },
-} satisfies Config;
+});
