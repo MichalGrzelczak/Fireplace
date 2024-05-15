96e4a17bfbd601c617fec8e11fa031fb856e01f0
Patryk Krasuski <patryk.krasuski@appfire.com>
2024-05-15 09:25:16 +0200

    debug redirects

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
+  console.log("token:", token);
 
   if (!token && process.env.NEXTAUTH_URL) {
     return NextResponse.redirect(new URL("/", request.url));
