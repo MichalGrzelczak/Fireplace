f742b80e091a4a1fc87dc243569a662b509f4760
Michał Grzelczak <michal.grzelczak@softwareplant.com>
2024-05-22 10:19:24 +0200

    remove email

--- app/app/projects/api-consts.ts
+++ app/app/projects/api-consts.ts
@@ -1,8 +1,7 @@
 export const TOKEN: string = process.env.JIRA_API_TOKEN ?? "";
+const JIRA_MAIL: string = process.env.EMAIL ?? "";
 export const HEADERS = {
-  Authorization: `Basic ${Buffer.from(
-    "michal.grzelczak@appfire.com:" + TOKEN,
-  ).toString("base64")}`,
+  Authorization: `Basic ${Buffer.from(JIRA_MAIL + ":" + TOKEN).toString("base64")}`,
   Accept: "application/json",
 };
 export const BASE_URL: string = "https://appfire.atlassian.net/rest/api/3";
